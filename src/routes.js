4721e06ddb737f22bbef0595c41e31357e4fc20d
Mercy <maritimmercy8@gmail.com>
2025-04-25 16:28:59 +0300

    added all

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,32 @@
-import Home from "./pages/Home";
-import Movie from "./pages/Movie";
 import Actors from "./pages/Actors";
-import Directors from "./pages/Directors";
+import Directors from "./pages/Directors"
+import Home from "./pages/Home"
+import Movie from "./pages/Movie"
 import ErrorPage from "./pages/ErrorPage";
-import { createRoutesFromElements, Route } from "react-router-dom";
 
-const routes = createRoutesFromElements([
+const routes = [
   {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/movie/:id",
     element: <Movie />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/directors",
     element: <Directors />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/actors",
     element: <Actors />,
-  },
-]);
-  
-    
+    errorElement: <ErrorPage />
+  }
+   
+  ];
 
 export default routes;
+
